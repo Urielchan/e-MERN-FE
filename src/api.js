17209e6926d421b55e0943d543658c3985446a9b
haikalnuril <haikal.nuril23@gmail.com>
2025-01-03 08:26:11 +0700

    init

--- src/api.js
+++ src/api.js
@@ -9,19 +9,4 @@ const customAPI = axios.create({
     }
 });
 
-// Add interceptor to handle CORS preflight
-customAPI.interceptors.request.use((config) => {
-    config.headers['Access-Control-Allow-Origin'] = 'https://e-commerce-self-seven.vercel.app/';
-    return config;
-});
-
-// Optional: Add response interceptor to log errors
-customAPI.interceptors.response.use(
-    response => response,
-    error => {
-        console.error('API Error:', error.response?.status, error.response?.data);
-        return Promise.reject(error);
-    }
-);
-
 export default customAPI;
