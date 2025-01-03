deb0888c03d8780541c88bd52f920b6f8401f7ab
haikalnuril <haikal.nuril23@gmail.com>
2025-01-03 08:36:39 +0700

    init

--- src/api.js
+++ src/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 const customAPI = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://e-mern-be.vercel.app/api/v1',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+    baseURL: "/api/v1",
 });
 
 export default customAPI;
